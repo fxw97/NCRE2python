b343a7c4c0672d6da183a27bc4e4d634aa06e95c
xuewei <1550505935@qq.com>
2021-09-23 23:25:04 +0800

    update

--- 第10套/综合应用题/10-46.demo.py
+++ 第10套/综合应用题/10-46.demo.py
@@ -12,10 +12,13 @@ while True:
         if count > 100:
             print('猜测超过5次，答题失败')
             break
-        if letter_input > letter:
-            print('你输入的字母在该字母之后')
-        if letter_input < letter:
-            print('你输入的字母在该字母之前：')
-        if letter_input == letter:
-            print('恭喜你猜对了，共猜了{}次'.format(count))
-            break
+        else:
+            if letter_input == letter:
+                print('恭喜你猜对了，共猜了{}次'.format(count))
+                break
+            elif letter_input > letter:
+                print('你输入的字母在该字母之后')
+            elif letter_input < letter:
+                print('你输入的字母在该字母之前')
+            else:
+                print('未知错误')
